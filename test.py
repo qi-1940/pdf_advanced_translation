1ba27fa6555da48a4c099eac56ba664af6f4dc66
qi-1940 <xin_hang_mu@qq.com>
2025-05-20 19:00:55 +0800

    7.1

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ import os
 
 # 构造文件路径
 current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_dir, 'test_pdfs/test5.pdf')  # 替换为你的文件名
+file_path = os.path.join(current_dir, 'test_pdfs/test1.pdf')  # 替换为你的文件名
 
-
-#translate_pdf('1.pdf')
 translate_pdf_ai(file_path)
--- translation.py
+++ translation.py
@@ -11,11 +11,11 @@ import requests
 import time
 import fitz 
 import os
-from ai2 import ai_blocks
+from ai3 import ai_pdf_process
 
 def baidu_translate(query):
-    app_id = '20250415002334058'
-    secret_key = 'SVA9Lv0mE90NQsj76c0W'
+    app_id = '20250425002342076'
+    secret_key = 'pT1QnotEaWuvpefNLgce'
     url = 'https://fanyi-api.baidu.com/api/trans/vip/translate'
     salt = str(round(time.time()))  # 随机数，这里使用了当前时间戳（秒级）
     sign_str = app_id + query + salt + secret_key
@@ -44,71 +44,87 @@ def to_plain_block(block):
             plain_block+=every_char
     return plain_block
 
-def is_english_letter(char):
-    return len(char) == 1 and char.isalpha() and char.isascii()
-
 def bbox_transform(bbox,x,y):
+    '''
+    传入的参数是pdf页面转图片后，识别出来的相对矩形位置(bbox),
+    以及pdf页面的宽度和长度
+    '''
     bbox[0] = bbox[0]*x
     bbox[2] = bbox[2]*x
     bbox[1] = bbox[1]*y
     bbox[3] = bbox[3]*y
     return bbox
-    
+
+def is_tag_textbox(tag_str):
+    if tag_str=='title' or \
+        tag_str=='abandon' or \
+        tag_str=='table_caption' \
+        or tag_str=='plain text':
+        return 1
+    else:
+        return 0 
+
 def translate_pdf_ai(input_pdf_path):
-    with fitz.open(input_pdf_path) as input_pdf:
-        output_pdf=fitz.Document()
-        for input_pdf_page in input_pdf:
-            
-            #获取当前页面的宽度和高度
-            input_pdf_page_width = input_pdf_page.rect.width  # 获取宽度（点）
-            input_pdf_page_height = input_pdf_page.rect.height  # 获取高度（点）
-            
-            #在要输出的文档中添加页面
-            output_pdf_current_page=output_pdf.new_page(width=input_pdf_page_width, height=input_pdf_page_height)
-            output_pdf_current_page.clean_contents()
-            
-            #把当前页面转化为temp.png
-            input_pdf_page_pixmap=input_pdf_page.get_pixmap(dpi=300)
-            input_pdf_page_pixmap.save('temp.png',output='png')
-            
-            #获取当前页面的所有文本块的pymupdf坐标，接着翻译、填充到要输出的文件的页面
-            blocks=ai_blocks('temp.png')
-            for each_block in blocks:
-                if each_block["class_id"]==1 or each_block["class_id"]==2 :
-                    each_block_bbox=bbox_transform(each_block["bbox_normalized"],input_pdf_page_width,input_pdf_page_height)
-                    each_block_rect=fitz.Rect(each_block_bbox[0],\
-                                            each_block_bbox[1],\
-                                            each_block_bbox[2],\
-                                            each_block_bbox[3])
-                if  each_block["class"]=='title':
-                    each_block_bbox=bbox_transform(each_block["bbox_normalized"],input_pdf_page_width,input_pdf_page_height)
-                    each_block_rect=fitz.Rect(each_block_bbox[0]-20,\
-                                            each_block_bbox[1],\
-                                            each_block_bbox[2]+5,\
-                                            each_block_bbox[3])
-                
-                each_block_dict_blocks = (input_pdf_page.get_text("dict", clip=each_block_rect))['blocks']
-                
-                if each_block_dict_blocks != [] and each_block_dict_blocks[0]['type']==0:
-                    each_block_fontsize = each_block_dict_blocks[0]['lines'][0]['spans'][0]['size']
-                    each_block_text=to_plain_block(input_pdf_page.get_text('text',clip=each_block_rect))
-                    each_block_tran=baidu_translate(each_block_text)[0]['dst']
-                    print(each_block_tran)
-                    print(each_block_fontsize)
 
-                    output_pdf_current_page.draw_rect(each_block_rect, color=(1, 0, 0)) 
-                    while output_pdf_current_page.insert_textbox(\
-                        rect=each_block_rect,\
-                        buffer=each_block_tran,\
-                        fontname='china-ss',\
-                        fontsize=each_block_fontsize-1) <0:
-                        each_block_rect[0]+=1
-                        each_block_rect[1]+=1
-                        each_block_rect[2]+=1
-                        each_block_rect[3]+=1
-                        each_block_fontsize-=1
+    #预处理（除了文字块外的所有处理）
+    if ai_pdf_process(input_pdf_path) == False:
+        print('预处理失败！')
+        return 0
+    
+    with fitz.open('output/temp.pdf') as temp_pdf:
+        with fitz.open(input_pdf_path) as input_pdf:
+            temp_page_num=0
+            for temp_pdf_page in temp_pdf:
+                input_pdf_page=input_pdf[temp_page_num]
+                #获取当前页面的宽度和高度
+                temp_pdf_page_width = temp_pdf_page.rect.width  # 获取宽度（点）
+                temp_pdf_page_height = temp_pdf_page.rect.height  # 获取高度（点）
 
-    output_pdf.save(f"{input_pdf_path}-中文翻译版.pdf")
-    output_pdf.close()
-    os.remove('temp.png')
+                #得到每个pdf页面的块列表
+                try:
+                    with open(f"output/json/{temp_page_num}.json", "r", encoding="utf-8") as temp_page_json_file:
+                        temp_page_blocks = json.load(temp_page_json_file)
+                        temp_page_num+=1
+
+                    #得到当前页面的文字块列表
+                    temp_page_text_blocks=[]
+                    for temp_page_block in temp_page_blocks:
+                        if is_tag_textbox(temp_page_block['class'])==1:
+                            temp_page_text_blocks.append(temp_page_block)
+
+                    #遍历当前页面的所有文字块
+                    for each_temp_page_text_block in temp_page_text_blocks:
+                        #得到当前文字块的pymupdf坐标
+                        if  each_temp_page_text_block["class"]=='title':
+                            each_block_bbox=bbox_transform(each_temp_page_text_block["bbox_normalized"],temp_pdf_page_width,temp_pdf_page_height)
+                            each_block_rect=fitz.Rect(each_block_bbox[0]-20,each_block_bbox[1],each_block_bbox[2]+5,each_block_bbox[3])
+                        else :
+                            each_block_bbox=bbox_transform(each_temp_page_text_block["bbox_normalized"],temp_pdf_page_width,temp_pdf_page_height)
+                            each_block_rect=fitz.Rect(each_block_bbox[0],each_block_bbox[1],each_block_bbox[2],each_block_bbox[3])
+                            
+                        each_block_dict_blocks = (input_pdf_page.get_text("dict", clip=each_block_rect))['blocks']
+                        #with open("temp.txt", "w", encoding="utf-8") as file:
+                        #    each_block_dict_blocks[0]
+                        
+                        #文字块字号提取、翻译与填充代码
+                        if each_block_dict_blocks != [] and each_block_dict_blocks[0]['type']==0:
+                            each_block_fontsize = each_block_dict_blocks[0]['lines'][0]['spans'][0]['size']
+                            each_block_text=to_plain_block(input_pdf_page.get_text('text',clip=each_block_rect))
+                            each_block_tran=baidu_translate(each_block_text)[0]['dst']
+                            #print(each_block_tran)
+                            #print(each_block_text)
+                            while temp_pdf_page.insert_textbox(rect=each_block_rect,buffer=each_block_tran,fontname='china-ss',\
+                                fontsize=each_block_fontsize-1) <0:
+                                each_block_rect[0]+=1
+                                each_block_rect[1]+=1
+                                each_block_rect[2]+=1
+                                each_block_rect[3]+=1
+                                each_block_fontsize-=1
+    
+                except FileNotFoundError:
+                    print("文件不存在！")
+                except json.JSONDecodeError:
+                    print("JSON 格式错误！")
+                
+        temp_pdf.save(f"{input_pdf_path}-中文翻译版.pdf")
     return 1
